b08700485a457be5fbea2ddac097c34343cb2352
jarrahi_mm <jarrahi.mm@outlook.com>
2020-01-01 23:29:27 +0330

    fetch comments data and view comments

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -1,101 +1,76 @@
 import React, {Component} from 'react';
-import {Button, Comment, Form, Header} from 'semantic-ui-react'
+import {Comment} from 'semantic-ui-react'
 import './Post.css'
+
+function processComment(props, comment) {
+    if (!comment.id.startsWith(props.startingId))
+        return null;
+    if (comment.id === props.startingId)
+        return null;
+    let extractedId = comment.id.replace(props.startingId, '');
+    return (!extractedId.includes('.')) ?
+        <React.Fragment>
+            <Comment>
+                <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/matt.jpg'/>
+                <Comment.Content>
+                    <Comment.Author as='a'>{comment.username}</Comment.Author>
+                    <Comment.Metadata>
+                        <div>Today at 5:42PM</div>
+                    </Comment.Metadata>
+                    <Comment.Text>{comment.text}</Comment.Text>
+                    <Comment.Actions>
+                        <Comment.Action>Reply</Comment.Action>
+                    </Comment.Actions>
+                </Comment.Content>
+                <Comments comments={props.comments} startingId={comment.id + '.'}/>
+            </Comment>
+        </React.Fragment> :
+        null;
+}
+
+function Comments(props) {
+    return (
+        <Comment.Group>
+            {props.comments.map(comment => processComment(props, comment))}
+        </Comment.Group>
+    )
+}
 
 
 class Post extends Component {
     constructor(probs) {
         super(probs);
-        this.state = {postId: probs.postId}
+        this.state = {
+            postId: probs.postId,
+            username: '',
+            time: '',
+            picURL: '',
+            video: '',
+            text: '',
+            comments: []
+        };
+    }
+
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    fetchData() {
+        this.setState({username: "Writer Username"});
+        this.setState({time: "Post Time"});
+        this.setState({picURL: "https://www.google.com/search?safe=strict&sxsrf=ACYBGNTLD7yk7UA0uEnWw51jzX5SFGVmVQ:1577901896679&q=New+Year%27s+Day&oi=ddle&ct=144862468&hl=en&sa=X&ved=0ahUKEwikvajQ_uLmAhWl0KYKHa9FCM0QPQgO"});
+        this.setState({video: "null"});
+        this.setState({text: "Text"});
+        let comments = [];
+        comments.push({username: 'username 1', id: '.1', text: 'comment 1'});
+        comments.push({username: 'username 2', id: '.2', text: 'comment 2'});
+        comments.push({username: 'username 1.1', id: '.1.1', text: 'comment 1-1'});
+        comments.push({username: 'username 1.2.1', id: '.1.2.1', text: 'comment 1-2-1'});
+        comments.push({username: 'username 1.2', id: '.1.2', text: 'comment 1-2'});
+        this.setState({comments: comments});
     }
 
     render() {
-        const CommentExampleComment = () => (
-            <Comment.Group>
-                <Header as='h3' dividing>
-                    Comments
-                </Header>
-
-                <Comment>
-                    <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/matt.jpg'/>
-                    <Comment.Content>
-                        <Comment.Author as='a'>Matt</Comment.Author>
-                        <Comment.Metadata>
-                            <div>Today at 5:42PM</div>
-                        </Comment.Metadata>
-                        <Comment.Text>How artistic!</Comment.Text>
-                        <Comment.Actions>
-                            <Comment.Action>Reply</Comment.Action>
-                        </Comment.Actions>
-                    </Comment.Content>
-                </Comment>
-
-                <Comment>
-                    <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg'/>
-                    <Comment.Content>
-                        <Comment.Author as='a'>Elliot Fu</Comment.Author>
-                        <Comment.Metadata>
-                            <div>Yesterday at 12:30AM</div>
-                        </Comment.Metadata>
-                        <Comment.Text>
-                            <p>This has been very useful for my research. Thanks as well!</p>
-                        </Comment.Text>
-                        <Comment.Actions>
-                            <Comment.Action>Reply</Comment.Action>
-                        </Comment.Actions>
-                    </Comment.Content>
-                    <Comment.Group>
-                        <Comment>
-                            <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/jenny.jpg'/>
-                            <Comment.Content>
-                                <Comment.Author as='a'>Jenny Hess</Comment.Author>
-                                <Comment.Metadata>
-                                    <div>Just now</div>
-                                </Comment.Metadata>
-                                <Comment.Text>Elliot you are always so right :)</Comment.Text>
-                                <Comment.Actions>
-                                    <Comment.Action>Reply</Comment.Action>
-                                </Comment.Actions>
-                            </Comment.Content>
-                            <Comment.Group>
-                                <Comment>
-                                    <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/jenny.jpg'/>
-                                    <Comment.Content>
-                                        <Comment.Author as='a'>Jenny Hess</Comment.Author>
-                                        <Comment.Metadata>
-                                            <div>Just now</div>
-                                        </Comment.Metadata>
-                                        <Comment.Text>Elliot you are always so right :)</Comment.Text>
-                                        <Comment.Actions>
-                                            <Comment.Action>Reply</Comment.Action>
-                                        </Comment.Actions>
-                                    </Comment.Content>
-                                </Comment>
-                            </Comment.Group>
-                        </Comment>
-                    </Comment.Group>
-                </Comment>
-
-                <Comment>
-                    <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/joe.jpg'/>
-                    <Comment.Content>
-                        <Comment.Author as='a'>Joe Henderson</Comment.Author>
-                        <Comment.Metadata>
-                            <div>5 days ago</div>
-                        </Comment.Metadata>
-                        <Comment.Text>Dude, this is awesome. Thanks so much</Comment.Text>
-                        <Comment.Actions>
-                            <Comment.Action>Reply</Comment.Action>
-                        </Comment.Actions>
-                    </Comment.Content>
-                </Comment>
-
-                <Form reply>
-                    <Form.TextArea/>
-                    <Button content='Add Reply' labelPosition='left' icon='edit' primary/>
-                </Form>
-            </Comment.Group>
-        );
         let containerWidth = Math.min(window.innerWidth, window.innerHeight);
         let horizontalMargin = (window.innerWidth - containerWidth) / 2;
         return (
@@ -104,7 +79,8 @@ class Post extends Component {
                 left: horizontalMargin
             }}>
                 PostId: {this.state.postId}
-                <CommentExampleComment/>
+                {/*<CommentExampleComment/>*/}
+                <Comments comments={this.state.comments} startingId='.'/>
             </div>
         );
     }
