98419a4eef2f3d36e67b97ea19e7cbb0487d6cc9
jarrahi_mm <jarrahi.mm@outlook.com>
2020-01-29 21:32:30 +0330

    add followers field to channel info
    Signed-off-by: jarrahi_mm <jarrahi.mm@outlook.com>

--- Back_Django/mainApp/models.py
+++ Back_Django/mainApp/models.py
@@ -6,6 +6,7 @@ class ChannelInfo(models.Model):
     channelId = models.CharField(max_length=16, blank=False, null=False, unique=True, primary_key=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, related_name='owningChannels')
     contributor = models.ManyToManyField(User, blank=True, related_name='contributedChannels')
+    followers = models.ManyToManyField(User, blank=True, related_name='followings')
     followersNum = models.IntegerField(default=0)
     followingsNum = models.IntegerField(default=0)
     postsNum = models.IntegerField(default=0)
--- Front_React/src/components/Login.js
+++ Front_React/src/components/Login.js
@@ -5,17 +5,24 @@ class Login extends Component {
     constructor(probs) {
         super(probs);
         this.state = {
-            credentials: {'username': '', 'password': ''}
+            loginCredentials: {'username': '', 'password': ''},
+            signInCredentials: {'username': '', 'password': '', 'firstName': '', 'lastName': '', 'email': ''}
         }
     }
-    
-    onType = event => {
-        let credentials = this.state.credentials;
+
+    changeLoginCred = event => {
+        let credentials = this.state.loginCredentials;
         credentials[event.target.name] = event.target.value;
-        this.setState({credentials: credentials});
+        this.setState({loginCredentials: credentials});
     };
 
-    submit = () => {
+    changeSignInCred = event => {
+        let credentials = this.state.signInCredentials;
+        credentials[event.target.name] = event.target.value;
+        this.setState({signInCredentials: credentials});
+    };
+
+    login = () => {
         fetch(`http://127.0.0.1:8000/login/`, {
             method: 'POST',
             headers: {
@@ -25,6 +32,7 @@ class Login extends Component {
         }).then(response => response.json())
             .then(response => {
                 this.props.cookies.set('myCookie', response.token);
+                window.location.href = '/';
             })
             .catch(error => {
             });
@@ -32,16 +40,48 @@ class Login extends Component {
 
     render() {
         return (
-            <div>
-                <label>
-                    Username:
-                    <input type='text' name='username' onChange={this.onType}/>
-                </label>
-                <label>
-                    Password:
-                    <input type='password' name='password' onChange={this.onType}/>
-                </label>
-                <button onClick={this.submit}>login</button>
+            <div className="ui placeholder segment">
+                <div className="ui stackable very relaxed two column grid">
+                    <div className="column">
+                        <form className="ui form">
+                            <div className="field">
+                                <label>Username</label>
+                                <div className="ui left icon input">
+                                    <input type="text" placeholder="Username" name='username' onChange={this.changeLoginCred}/>
+                                    <i aria-hidden="true" className="user icon"/>
+                                </div>
+                            </div>
+                            <div className="field">
+                                <label>Password</label>
+                                <div className="ui left icon input">
+                                    <input type="password" name='password' onChange={this.changeLoginCred}/>
+                                    <i aria-hidden="true" className="lock icon"/>
+                                </div>
+                            </div>
+                            <button className="ui primary button" onClick={this.login}>Login</button>
+                        </form>
+                    </div>
+                    <div className="column">
+                        <form className="ui form">
+                            <div className="field">
+                                <label>Username</label>
+                                <div className="ui left icon input">
+                                    <input type="text" placeholder="Username" name='username' onChange={this.changeLoginCred}/>
+                                    <i aria-hidden="true" className="user icon"/>
+                                </div>
+                            </div>
+                            <div className="field">
+                                <label>Password</label>
+                                <div className="ui left icon input">
+                                    <input type="password" name='password' onChange={this.changeLoginCred}/>
+                                    <i aria-hidden="true" className="lock icon"/>
+                                </div>
+                            </div>
+                            <button className="ui primary button" onClick={this.login}>Login</button>
+                        </form>
+                    </div>
+                </div>
+                <div className="ui vertical divider">Or</div>
             </div>
         )
     }
