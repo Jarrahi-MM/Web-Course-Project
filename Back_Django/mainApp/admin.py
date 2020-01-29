9ea0ddcf160a686e03d2fe0b7b2aa082c4ff2cc4
Jarrahi_MM <jarrahi.mm@gmail.com>
2020-01-29 17:15:36 +0330

    First Design Of Models Finished
    Signed-off-by: jarrahi_mm <jarrahi.mm@outlook.com>

--- Back_Django/mainApp/admin.py
+++ Back_Django/mainApp/admin.py
@@ -1,1 +1,9 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import PostLike, CommentLike, ChannelInfo, Post, Comment
+
+admin.site.register(ChannelInfo)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(PostLike)
+admin.site.register(CommentLike)
--- Back_Django/mainApp/models.py
+++ Back_Django/mainApp/models.py
@@ -4,25 +4,55 @@ from django.db import models
 
 class ChannelInfo(models.Model):
     channelId = models.CharField(max_length=16, blank=False, null=False, unique=True, primary_key=True)
-    owner = models.ForeignKey(User, on_delete='CASCADE', blank=False, related_name='owningChannels')
-    contributor = models.ManyToManyField(User, blank=True, null=True, related_name='contributedChannels')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, related_name='owningChannels')
+    contributor = models.ManyToManyField(User, blank=True, related_name='contributedChannels')
     followersNum = models.IntegerField(default=0)
     followingsNum = models.IntegerField(default=0)
     postsNum = models.IntegerField(default=0)
     isPersonal = models.BooleanField(null=False, blank=False)
 
 
+class Comment(models.Model):
+    commentNumber = models.IntegerField()
+    supComment = models.ForeignKey("self", on_delete=models.CASCADE)
+    text = models.TextField()
+    likesNum = models.IntegerField()
+    creationDate = models.DateField(blank=False)
+
+    class Meta:
+        unique_together = ['supComment', 'commentNumber']
+        index_together = ['supComment', 'commentNumber']
+
+
 class Post(models.Model):
-    postId = models.IntegerField(primary_key=True, auto_created=True)
-    channel = models.ForeignKey(ChannelInfo, blank=False, null=False, on_delete="CASCADE", related_name='posts')
-    creator = models.ForeignKey(User, blank=False, null=False, on_delete="SET_NULL")
+    postNumber = models.IntegerField()
+    channel = models.ForeignKey(ChannelInfo, blank=False, null=False, on_delete=models.CASCADE, related_name='posts')
+    creator = models.ForeignKey(User, blank=False, null=True, on_delete=models.SET_NULL)
     creationDate = models.DateField(blank=False)
     updateVal = models.IntegerField(default=0, blank=False)
+    firstComment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    likesNum = models.IntegerField()
+
+    class Meta:
+        unique_together = ['channel', 'postNumber']
+        index_together = ['channel', 'postNumber']
 
 
-class Like(models.Model):
-    user = models.ForeignKey(User, blank=False, null=False, on_delete='CASCADE', related_name='likes')
-    post = models.ForeignKey(Post, blank=False, null=False, on_delete='CASCADE', related_name='likes')
+class PostLike(models.Model):
+    user = models.ForeignKey(User, blank=False, null=False, on_delete=models.CASCADE, related_name='postLikes')
+    post = models.ForeignKey(Post, blank=False, null=False, on_delete=models.CASCADE, related_name='likes')
     isPositive = models.BooleanField(default=True)
 
+    class Meta:
+        unique_together = ['user', 'post']
+        index_together = ['user', 'post']
 
+
+class CommentLike(models.Model):
+    user = models.ForeignKey(User, blank=False, null=False, on_delete=models.CASCADE, related_name='commentLikes')
+    comment = models.ForeignKey(Comment, blank=False, null=False, on_delete=models.CASCADE, related_name='likes')
+    isPositive = models.BooleanField(default=True)
+
+    class Meta:
+        unique_together = ['user', 'comment']
+        index_together = ['user', 'comment']
