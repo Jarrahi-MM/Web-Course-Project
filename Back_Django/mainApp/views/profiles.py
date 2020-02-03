1466edc918f61ec8d6ff2393fcd5f6b40ec0362b
amir <amirmoini78@gmail.com>
2020-02-03 15:22:57 +0330

    fixed crash when not found: profiles.py

--- Back_Django/mainApp/views/profiles.py
+++ Back_Django/mainApp/views/profiles.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
-from rest_framework import status, authentication,permissions
+from rest_framework import status, authentication, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
 
 from ..models import ProfileInfo
 from ..serializers import ProfileSerializer
@@ -13,20 +14,22 @@ class Profiles(APIView):
 
     @staticmethod
     def get(request, username):
-        try:
-            user = User.objects.get(username=username)
-            profile = ProfileInfo.objects.get(user=user)
-        except ProfileInfo.DoesNotExist:  # Todo:Negin Check
-            return Response('Invalid user', status=status.HTTP_400_BAD_REQUEST)
+        # try:
+        #     user = User.objects.get(username=username)
+        #     profile = ProfileInfo.objects.get(user=user)
+        # except ProfileInfo.DoesNotExist:  # Todo:Negin Check
+        #     return Response('Invalid user', status=status.HTTP_400_BAD_REQUEST)
+        profile = get_object_or_404(ProfileInfo, user__username=username)
         return Response(ProfileSerializer(profile).data, status.HTTP_200_OK)
 
     @staticmethod
     def put(request, username):
-        try:
-            user = User.objects.get(username=username)
-            profile = ProfileInfo.objects.get(user=user)
-        except ProfileInfo.DoesNotExist:  # Todo:Negin Check
-            return Response('Invalid user', status=status.HTTP_400_BAD_REQUEST)
+        # try:
+        #     user = User.objects.get(username=username)
+        #     profile = ProfileInfo.objects.get(user=user)
+        # except ProfileInfo.DoesNotExist:  # Todo:Negin Check
+        #     return Response('Invalid user', status=status.HTTP_400_BAD_REQUEST)
+        profile = get_object_or_404(ProfileInfo, user__username=username)
         if request.user.is_anonymous:
             return Response("get the hell out", status=status.HTTP_400_BAD_REQUEST)  # Todo:Negin Check
         if request.user.username != username:
