a345a5f2f1e448b44610863cc335dd1214ceb9d3
jarrahi_mm <jarrahi.mm@outlook.com>
2020-02-03 18:05:20 +0330

    put and delete comment
    Signed-off-by: jarrahi_mm <jarrahi.mm@outlook.com>

--- Back_Django/mainApp/views/comments.py
+++ Back_Django/mainApp/views/comments.py
@@ -14,70 +14,64 @@ class CommentView(APIView):
     @staticmethod
     def get(request, ):
         try:
-            supComment = Comment.objects.get(id=request.data['fatherId'])
+            sup_comment = Comment.objects.get(id=request.data['fatherId'])
         except Comment.DoesNotExist:
             return Response('Invalid Father', status=status.HTTP_400_BAD_REQUEST)
         if request.user.is_anonymous:
             return Response("You're not logged in", status=status.HTTP_400_BAD_REQUEST)
 
-        comments = supComment.subComments.filter(commentNumber__gte=request.data['from'],
-                                                 commentNumber__lt=request.data['to'])
+        comments = sup_comment.subComments.filter(commentNumber__gte=request.data['from'],
+                                                  commentNumber__lt=request.data['to'])
         return Response(CommentSerializer(comments, many=True).data, status=status.HTTP_201_CREATED)
 
     @staticmethod
     def post(request):
         try:
-            supComment = Comment.objects.get(id=request.data['fatherId'])
+            sup_comment = Comment.objects.get(id=request.data['fatherId'])
         except Comment.DoesNotExist:
             return Response('Invalid Father', status=status.HTTP_400_BAD_REQUEST)
         if request.user.is_anonymous:
             return Response("You're not logged in", status=status.HTTP_400_BAD_REQUEST)
 
-        comment = Comment.objects.create(commentNumber=supComment.subCommentsNum + 1, supComment=supComment,
+        comment = Comment.objects.create(commentNumber=sup_comment.subCommentsNum + 1, supComment=sup_comment,
                                          creator=request.user, text=request.data['text'], likesNum=0, subCommentsNum=0,
                                          creationDate=datetime.now())
 
-        supComment.subCommentsNum += 1
+        sup_comment.subCommentsNum += 1
         comment.save()
-        supComment.save()
+        sup_comment.save()
         return Response(CommentSerializer(comment, many=False).data, status=status.HTTP_201_CREATED)
 
-    # @staticmethod
-    # def put(request):
-    #     try:
-    #         channel = Channel.objects.get(channelId=channelId)
-    #     except Channel.DoesNotExist:
-    #         return Response('Invalid channel', status=status.HTTP_400_BAD_REQUEST)
-    #     try:
-    #         post = channel.posts.get(postNumber=postNumber)
-    #     except Post.DoesNotExist:
-    #         return Response('Invalid post number', status=status.HTTP_400_BAD_REQUEST)
-    #     if request.user.is_anonymous:
-    #         return Response("You're not logged in", status=status.HTTP_400_BAD_REQUEST)
-    #     if (request.user.username != post.creator.username) and (
-    #             request.user.username != channel.owner.username):
-    #         return Response("You can't delete the post", status=status.HTTP_400_BAD_REQUEST)
-    #     post.creationDate = datetime.now()
-    #     post.updateVal += 1
-    #     post.image = request.data['image']
-    #     post.text = request.data['text']
-    #     post.save()
-    #     return Response(PostSerializer(post, many=False).data, status=status.HTTP_400_BAD_REQUEST)
+    @staticmethod
+    def put(request):
+        try:
+            comment = Comment.objects.get(id=request.data['id'])
+        except Comment.DoesNotExist:
+            return Response('Invalid Comment', status=status.HTTP_400_BAD_REQUEST)
+        if request.user.is_anonymous:
+            return Response("You're not logged in", status=status.HTTP_400_BAD_REQUEST)
+        if request.user.username != comment.creator.username:
+            return Response("You can not edit this comment", status=status.HTTP_400_BAD_REQUEST)
 
-    # @staticmethod
-    # def delete(request):
-    #     try:
-    #         channel = Channel.objects.get(channelId=channelId)
-    #     except Channel.DoesNotExist:
-    #         return Response('Invalid channel', status=status.HTTP_400_BAD_REQUEST)
-    #     try:
-    #         post = channel.posts.get(postNumber=postNumber)
-    #     except Post.DoesNotExist:
-    #         return Response('Invalid post number', status=status.HTTP_400_BAD_REQUEST)
-    #     if request.user.is_anonymous:
-    #         return Response("You're not logged in", status=status.HTTP_400_BAD_REQUEST)
-    #     if (request.user.username != post.creator.username) and (
-    #             request.user.username != channel.owner.username):
-    #         return Response("You can't delete the post", status=status.HTTP_400_BAD_REQUEST)
-    #     post.delete()
-    #     return Response('', status.HTTP_200_OK)
+        comment.text = request.data['text']
+        comment.creationDate = datetime.now()
+        comment.save()
+
+        return Response(CommentSerializer(comment, many=False).data, status=status.HTTP_201_CREATED)
+
+    @staticmethod
+    def delete(request):
+        try:
+            comment = Comment.objects.get(id=request.data['id'])
+        except Comment.DoesNotExist:
+            return Response('Invalid Comment', status=status.HTTP_400_BAD_REQUEST)
+        if request.user.is_anonymous:
+            return Response("You're not logged in", status=status.HTTP_400_BAD_REQUEST)
+        if request.user.username != comment.creator.username:
+            return Response("You can not delete this comment", status=status.HTTP_400_BAD_REQUEST)
+
+        comment.text = 'deleted'  # Todo: Think
+        comment.creationDate = datetime.now()
+        comment.save()
+
+        return Response(CommentSerializer(comment, many=False).data, status=status.HTTP_201_CREATED)
