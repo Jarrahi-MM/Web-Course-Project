48e0721121698a14131a59911c8690173b74f20f
neginjafariii <neginjfi79@gmail.com>
2020-02-05 15:40:53 +0330

    add Conributor updating

--- Front_React/src/components/channel/editChannelContributors.js
+++ Front_React/src/components/channel/editChannelContributors.js
@@ -3,10 +3,31 @@ import React, {Component} from "react";
 
 class EditChannelContributors extends Component {
 
+    newContributorAdded = event => {
+
+    };
+
+    addClicked = ev => {
+
+    };
+
     render() {
         return (
             <React.Fragment>
-                <h3>guguliiii</h3>
+                <form className="ui form">
+                    <div className="field">
+                        <div className="three fields">
+                            <div className="field">
+                                <label>Add New Contributor</label>
+                                <input type="text" name="newContributor" placeholder="new Contributor"
+                                       onChange={this.newContributorAdded}/>
+                            </div>
+                            <button className="ui button mini blue" onClick={this.addClicked}>
+                                Add
+                            </button>
+                        </div>
+                    </div>
+                </form>
             </React.Fragment>
         )
     }
