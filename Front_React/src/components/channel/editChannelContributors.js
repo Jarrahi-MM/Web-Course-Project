b62e7353a3fc08ce893846242c871115e8fc932c
neginjafariii <neginjfi79@gmail.com>
2020-02-05 17:44:19 +0330

    manage contributor updating

--- Front_React/src/components/channel/editChannelContributors.js
+++ Front_React/src/components/channel/editChannelContributors.js
@@ -1,13 +1,35 @@
 import React, {Component} from "react";
+import {Link} from "react-router-dom";
+
+const containStyle = {
+    width: '50%',
+    background: '#a4deff'
+};
+const contain2Style = {
+    width: '10%',
+    background: '#ff728a'
+};
 
 
 class EditChannelContributors extends Component {
+
+    state = {
+        contributors: [
+            {
+                username: 'negin',
+            },
+        ]
+    };
 
     newContributorAdded = event => {
 
     };
 
     addClicked = ev => {
+
+    };
+
+    deleteClicked = ev => {
 
     };
 
@@ -25,9 +47,24 @@ class EditChannelContributors extends Component {
                             <button className="ui button mini blue" onClick={this.addClicked}>
                                 Add
                             </button>
+
                         </div>
                     </div>
                 </form>
+                <div>{this.state.contributors.map(contributor => {
+                    return (
+                        <div key={contributor.id} className="blue">
+                            <Link to={`/profile/${contributor.username}`}>
+                                <button className=" ui button  big" style={containStyle}>
+                                    <span>{contributor.username}</span>
+                                </button>
+                            </Link>
+                            <i className=" icon big trash" onClick={this.deleteClicked}/>
+                            <br/>
+                        </div>
+                    )
+                })}
+                </div>
             </React.Fragment>
         )
     }
