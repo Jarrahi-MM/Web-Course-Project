3ad02e8977103443f513b2057e0e0cb7b4f07010
neginjafariii <neginjfi79@gmail.com>
2020-02-05 09:04:37 +0330

    channel post added

--- Back_Django/mainApp/views/channels.py
+++ Back_Django/mainApp/views/channels.py
@@ -19,3 +19,16 @@ class Channels(APIView):
 
         serializer = ChannelSerializer(channels, many=True)
         return Response(serializer.data, status.HTTP_200_OK)
+
+    @staticmethod
+    def post(request):
+        try:
+            channel = Channel.objects.get(channelId=request.data['channelId'])
+        except Channel.DoesNotExist:
+            channel = Channel.objects.create(owner=request.user, channelName=request.data['channelName'],
+                                             channelId=request.data['channelId'], isPersonal=False,
+                                             followersNum=0, followingsNum=0,
+                                             postsNum=0, description=request.data['description'])
+            channel.save()
+            return Response('channel created', status=status.HTTP_201_CREATED)
+        return Response('this channel id is taken before', status=status.HTTP_400_BAD_REQUEST)
