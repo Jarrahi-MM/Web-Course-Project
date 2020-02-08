4e798c88d1604b4755c268c9cb05eb210134f0b6
amir <amirmoini78@gmail.com>
2020-02-08 20:52:06 +0330

    enhanced theming

--- Front_React/src/App.js
+++ Front_React/src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter, Route, Switch, withRouter} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Navbar from "./components/navbar/Navbar";
 import Profile from "./components/Profile/profile";
 import EditProfile from "./components/Profile/editProfile";
@@ -21,6 +21,8 @@ import FollowList from "./components/followList";
 import EditProfilePic from "./components/Profile/editProfilePic";
 import NotFound from "./components/NotFound";
 import BlockList from "./components/Profile/blockList";
+import {ThemeProvider} from "styled-components";
+import theme from "./globalStyles/Theme";
 
 
 class App extends Component {
@@ -37,100 +39,102 @@ class App extends Component {
 
     render() {
         return (
-            <BrowserRouter>
-                <CookiesProvider>
-                    <Route path={'/'}>
-                        <Navbar/>
-                        <EditorModal/>
-                    </Route>
-                    <Switch>
-                        <Route path={'/login'}>
-                            <Login/>
+            <ThemeProvider theme={theme}>
+                <BrowserRouter>
+                    <CookiesProvider>
+                        <Route path={'/'}>
+                            <Navbar/>
+                            <EditorModal/>
                         </Route>
-                        <Route path='/post/:channelId/:postNum' render={({match}) => {
-                            return (
-                                <div>
-                                    <Post channelId={match.params.channelId} postNum={match.params.postNum}
-                                          username={this.state.username} token={this.state.token}/>
-                                </div>
-                            );
-                        }}/>
-                        <Route exact path={'/'}>
-                            <Homepage/>
-                        </Route>
-                        <Route path='/profile/:username' render={({match}) => {
-                            return (
-                                <div>
-                                    <Profile username={match.params.username}
-                                             myAccount={(this.state.username === match.params.username)}/>
-                                </div>
-                            );
-                        }}/>
-                        <Route path='/followersList/:username' render={({match}) => {
-                            return (
-                                <div>
-                                    <FollowList username={match.params.username} follower={true}/>
-                                </div>
-                            );
-                        }}/>
-                        <Route path='/followingsList/:username' render={({match}) => {
-                            return (
-                                <div>
-                                    <FollowList username={match.params.username} follower={false}/>
-                                </div>
-                            );
-                        }}/>
-                        <Route path='/blockedUsers/:username' render={({match}) => {
-                            return (
-                                <div>
-                                    <BlockList username={match.params.username}/>
-                                </div>
-                            );
-                        }}/>
-                        <Route path={'/createChannel'}>
-                            <CreateChannel/>
-                        </Route>
-                        <Route path={'/channel'}>
-                            <Channel/>
-                        </Route>
-                        <Route path={'/createChannel'}>
-                            <Channel/>
-                        </Route>
-                        <Route path='/editProfile/:username' render={({match}) => {
-                            return (
-                                <div>
-                                    <EditProfile username={match.params.username}/>
-                                </div>
-                            );
-                        }}/>
-                        <Route path={'/alerts'}>
-                            <AlertsPage/>
-                        </Route>
+                        <Switch>
+                            <Route path={'/login'}>
+                                <Login/>
+                            </Route>
+                            <Route path='/post/:channelId/:postNum' render={({match}) => {
+                                return (
+                                    <div>
+                                        <Post channelId={match.params.channelId} postNum={match.params.postNum}
+                                              username={this.state.username} token={this.state.token}/>
+                                    </div>
+                                );
+                            }}/>
+                            <Route exact path={'/'}>
+                                <Homepage/>
+                            </Route>
+                            <Route path='/profile/:username' render={({match}) => {
+                                return (
+                                    <div>
+                                        <Profile username={match.params.username}
+                                                 myAccount={(this.state.username === match.params.username)}/>
+                                    </div>
+                                );
+                            }}/>
+                            <Route path='/followersList/:username' render={({match}) => {
+                                return (
+                                    <div>
+                                        <FollowList username={match.params.username} follower={true}/>
+                                    </div>
+                                );
+                            }}/>
+                            <Route path='/followingsList/:username' render={({match}) => {
+                                return (
+                                    <div>
+                                        <FollowList username={match.params.username} follower={false}/>
+                                    </div>
+                                );
+                            }}/>
+                            <Route path='/blockedUsers/:username' render={({match}) => {
+                                return (
+                                    <div>
+                                        <BlockList username={match.params.username}/>
+                                    </div>
+                                );
+                            }}/>
+                            <Route path={'/createChannel'}>
+                                <CreateChannel/>
+                            </Route>
+                            <Route path={'/channel'}>
+                                <Channel/>
+                            </Route>
+                            <Route path={'/createChannel'}>
+                                <Channel/>
+                            </Route>
+                            <Route path='/editProfile/:username' render={({match}) => {
+                                return (
+                                    <div>
+                                        <EditProfile username={match.params.username}/>
+                                    </div>
+                                );
+                            }}/>
+                            <Route path={'/alerts'}>
+                                <AlertsPage/>
+                            </Route>
 
 
-                        <Route path={'/fortest_editor'}>
-                            <Editor/>
-                        </Route>
-                        <Route path={'/fortest_postcard'}>
-                            <PostCard/>
-                        </Route>
-                        <Route path={'/fortest_modal'}>
-                            <Button
-                                onClick={() => this.props.openModal(
-                                    'comment_create',
-                                    {supCommentId: 25},
-                                )}
-                            >hiie</Button>
-                        </Route>
-                        <Route path={'/fortest_epp'}>
-                            <EditProfilePic/>
-                        </Route>
-                        <Route path={''}>
-                            <NotFound/>
-                        </Route>
-                    </Switch>
-                </CookiesProvider>
-            </BrowserRouter>
+                            <Route path={'/fortest_editor'}>
+                                <Editor/>
+                            </Route>
+                            <Route path={'/fortest_postcard'}>
+                                <PostCard/>
+                            </Route>
+                            <Route path={'/fortest_modal'}>
+                                <Button
+                                    onClick={() => this.props.openModal(
+                                        'comment_create',
+                                        {supCommentId: 25},
+                                    )}
+                                >hiie</Button>
+                            </Route>
+                            <Route path={'/fortest_epp'}>
+                                <EditProfilePic/>
+                            </Route>
+                            <Route path={''}>
+                                <NotFound/>
+                            </Route>
+                        </Switch>
+                    </CookiesProvider>
+                </BrowserRouter>
+            </ThemeProvider>
         );
     }
 
